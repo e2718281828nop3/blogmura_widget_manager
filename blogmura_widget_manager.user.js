3df4204ccade23fc7611b221dda8695ca9184d08
Masato Ishimoto <masato.ishimoto@gmail.com>
2017-12-27 02:05:56 +0900

    delete indent for userscript.org

--- blogmura_widget_manager.user.js
+++ blogmura_widget_manager.user.js
@@ -2,21 +2,21 @@
 
     'use strict';
 
-    // ==UserScript==
-    // @name         Blogmura Widget Manager
-    // @namespace    https://github.com/e2718281828nop3/
-    // @version      0.2
-    // @description  manage blogmura widgets
-    // @author       e2718281828nop3
-    // @copyright    2017, e2718281828nop3 (https://github.com/e2718281828nop3/)
-    // @license      GPL-3.0
-    // @match        http*://*.blogmura.com/*
-    // @require      https://github.com/e2718281828nop3/javascripts/raw/master/utilities/ext/object.js
-    // @require      https://github.com/e2718281828nop3/javascripts/raw/master/utilities/ext/array.js
-    // @require      https://github.com/e2718281828nop3/javascripts/raw/master/utilities/ext/dom.js
-    // @require      https://github.com/e2718281828nop3/javascripts/raw/master/utilities/class/storage.js
-    // @require      https://github.com/e2718281828nop3/blogmura_widget_manager/raw/master/widget_manager.js
-    // ==/UserScript==
+// ==UserScript==
+// @name         Blogmura Widget Manager
+// @namespace    https://github.com/e2718281828nop3/
+// @version      0.2
+// @description  manage blogmura widgets
+// @author       e2718281828nop3
+// @copyright    2017, e2718281828nop3 (https://github.com/e2718281828nop3/)
+// @license      GPL-3.0
+// @match        http*://*.blogmura.com/*
+// @require      https://github.com/e2718281828nop3/javascripts/raw/master/utilities/ext/object.js
+// @require      https://github.com/e2718281828nop3/javascripts/raw/master/utilities/ext/array.js
+// @require      https://github.com/e2718281828nop3/javascripts/raw/master/utilities/ext/dom.js
+// @require      https://github.com/e2718281828nop3/javascripts/raw/master/utilities/class/storage.js
+// @require      https://github.com/e2718281828nop3/blogmura_widget_manager/raw/master/widget_manager.js
+// ==/UserScript==
 
     const APP_NAME = 'BlogmuraWidgetManager';
 
